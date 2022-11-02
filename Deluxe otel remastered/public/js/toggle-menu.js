0073101c2fcf2d81b1171a7d6b10a2c35e308d34
Kerem Akın Soyak <80275467+AidenVanya@users.noreply.github.com>
2022-11-02 13:59:28 +0300

    Otel Sitesi
    Tanıtım amaçlı bir otel sitesi

---
+++ Deluxe otel remastered/public/js/toggle-menu.js
@@ -0,0 +1,6 @@
+function DisplayMenu() {
+    var link = document.getElementById("main-nav");
+    link.classList.toggle("mobile");
+    var link = document.getElementById("home");
+    link.classList.toggle("mobile");
+}
